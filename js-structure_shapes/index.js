5a22959c0967e1d97d06ebe904b995e07d5181d8
Hauke <hank.la.boon@gmail.com>
2024-11-11 19:14:37 +0100

    finish part 1

--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -5,14 +5,20 @@ const root = document.getElementById("root");
 
 const circle = document.createElement("div");
 circle.classList.add("circle");
-circle.addEventListener("click", getRandomColor);
+circle.addEventListener("click", () => {
+  circle.style.backgroundColor = getRandomColor();
+});
 
 const square = document.createElement("div");
 square.classList.add("square");
-square.addEventListener("click", getRandomColor);
+square.addEventListener("click", () => {
+  square.style.backgroundColor = getRandomColor();
+});
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", getRandomColor);
+pentagon.addEventListener("click", () => {
+  pentagon.style.backgroundColor = getRandomColor();
+});
 
 root.append(circle, square, pentagon);
