3c4bd142c03c45c6e2c9d0919b58093ff7ca1471
Hauke <hank.la.boon@gmail.com>
2024-11-19 15:33:06 +0100

    finish challenge

---
+++ react-state-1_box/src/App.jsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  // let isActive = false;
+  const [isActive, setIsActive] = useState(false);
+
+  console.log(isActive);
+  function handleClick() {
+    setIsActive(!isActive);
+    // Check that the value changes correctly.
+  }
+
+  return (
+    <main>
+      <div className={`box ${isActive ? "box--active" : ""}`} />
+      <button onClick={handleClick}>
+        {isActive ? "deactivate" : "activate"}
+      </button>
+    </main>
+  );
+}
