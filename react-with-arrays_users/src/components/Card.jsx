58e52169f8c6cec681a61f93057051b481f90d29
Hauke <hank.la.boon@gmail.com>
2024-11-20 11:32:31 +0100

    finish challenge

---
+++ react-with-arrays_users/src/components/Card.jsx
@@ -0,0 +1,25 @@
+import "./Card.css";
+import Tag from "./Tag";
+import Button from "./Button";
+function Card({ user }) {
+  return (
+    <article className="card">
+      <h2>{user.name}</h2>
+      <ul className="card__taglist">
+        {/* <Tag tag={user.roles[0]} /> */}
+        {user.roles.map((role) => {
+          return (
+            <Tag key={role} tag={role}>
+              role
+            </Tag>
+          );
+        })}
+      </ul>
+      <p>{user.about}</p>
+      <Button>edit</Button>
+      <Button buttonType="danger">invoice</Button>
+    </article>
+  );
+}
+
+export default Card;
