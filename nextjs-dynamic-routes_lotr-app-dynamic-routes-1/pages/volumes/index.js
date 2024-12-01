57ef9161cc410dcbaff8b6ae80d3d6b0b36fa0cb
Hauke <hank.la.boon@gmail.com>
2024-12-01 17:03:58 +0100

    add randomizer

--- nextjs-dynamic-routes_lotr-app-dynamic-routes-1/pages/volumes/index.js
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes-1/pages/volumes/index.js
@@ -1,10 +1,21 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
 import { volumes } from "@/lib/data.js";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
-  console.log(volumes);
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handlerFunction() {
+    let randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+
+  console.log("random volume :", getRandomElement(volumes));
   return (
     <>
       <h1>The Lord of the Rings</h1>
@@ -18,28 +29,7 @@ export default function Volumes() {
           );
         })}
       </ul>
+      <button onClick={handlerFunction}>random page</button>
     </>
   );
 }
-
-// return (
-//   <>
-//     <h1>The Lord of the Rings</h1>
-//     <p>{introduction}</p>
-//     <ul>
-//       <li>
-//         <Link href="/volumes/the-fellowship-of-the-ring">
-//           The Fellowship of the Ring
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/volumes/the-two-towers">The Two Towers</Link>
-//       </li>
-//       <li>
-//         <Link href="/volumes/the-return-of-the-king">
-//           The Return of the King
-//         </Link>
-//       </li>
-//     </ul>
-//   </>
-// );
