0c47a25fbe553fc9dda5270599619ae8efd10305
Hauke <hank.la.boon@gmail.com>
2024-12-02 11:40:06 +0100

    finish challenge

---
+++ js-unit-testing_get-animal/index.js
@@ -0,0 +1,9 @@
+export function getAnimal(animal) {
+  if (!animal) {
+    return `I do not like animals at all!`;
+  } else if (animal && animal[animal.length - 1] === "s" && animal !== "cats") {
+    return `I like ${animal}!`;
+  } else {
+    return `I totally love ${animal}!`;
+  }
+}
