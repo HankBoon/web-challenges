560ec611fb47322698777c543dd8d5195d739507
Hauke <hank.la.boon@gmail.com>
2024-12-10 17:39:34 +0100

    finish challenge

---
+++ backend-update-and-delete_products/pages/[id].js
@@ -0,0 +1,78 @@
+import useSWR from "swr";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
+
+export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`/api/products/${id}`, { method: "DELETE" });
+    if (response.ok) {
+      // await response.json();
+      router.push("/");
+    }
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      <StyledLink href="/">Back to all</StyledLink>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Update
+      </button>
+      <button type="button" onClick={() => handleDeleteProduct(id)}>
+        delete
+      </button>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct}></ProductForm>}
+    </ProductCard>
+  );
+}
+
+const ProductCard = styled.article`
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 1px 5px -2px var(--color-granite);
+`;
